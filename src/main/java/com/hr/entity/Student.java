aa856fd8f1c8730e04a03aedfdcfa4a8b15171ed
wsqtt <2122482248@qq.com>
2020-05-25 09:45:08 +0800

    测试git上传项目

---
+++ src/main/java/com/hr/entity/Student.java
@@ -0,0 +1,23 @@
+package com.hr.entity;
+
+public class Student {
+    private String sname;
+
+    private int sage;
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public int getSage() {
+        return sage;
+    }
+
+    public void setSage(int sage) {
+        this.sage = sage;
+    }
+}
